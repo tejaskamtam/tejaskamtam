06d71d3800635f9ec78d2d621385f7ca44892fd2
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-08-03 17:46:38 -0500

    reconfigure to bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+tabnine:config
+
+import { configure } from '@testing-library/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div class="App">
+      Hello There
+    </div>
+  );
+}
+
+export default App;
